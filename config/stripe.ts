baf78ac8d52abe561c93656a852e60176261925d
Caio Oliveira <caio.goncalves.pro@gmail.com>
2022-01-29 03:42:57 -0300

    second commit

---
+++ config/stripe.ts
@@ -0,0 +1,4 @@
+export default {
+  publickey: `pk_test_51KN939LlEvisGjMkFpAfs1Y7Fl7LbO0UPu0EntKZSyNlRCp0Ueg8ezLOrgAZnR5rqGjmuflhrWoJ4vHxLPDIujQD00YYg6LlSv`,
+  secretkey: `sk_test_51KN939LlEvisGjMk8lCsa26PsOMMgOB0Ri1X8EMTiHvl8SCfJV0ykyfi9A8FYGOPRgdznUFTof0F2dJfWGsF4Snw00lqDw3g85`,
+};
